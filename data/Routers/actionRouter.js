ecd641405628460015840f7ab9a316c468b863b1
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-11-11 15:53:41 -0600

    ID validation middleware and request.

--- data/Routers/actionRouter.js
+++ data/Routers/actionRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const Action = require("../data/helpers/actionModel");
 
+//Get request
 router.get("/", (req, res) => {
   Action.get()
     .then(action => {
@@ -12,5 +13,30 @@ router.get("/", (req, res) => {
     });
 });
 
+//Get by ID with validation middleware
+router.get("/:id", validateUserId, (req, res) => {
+  const id = req.params.id;
+  res.status(200).json(req.user);
+});
+
 const router = express.Router();
 module.exports = router;
+
+//ID Validation middleware
+function validateUserId(req, res, next) {
+  const id = req.params.id;
+  Action.get(id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res
+          .status(400)
+          .json({ message: "YOU ARE NOT A REAL USER WITH AN ID@" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "THIS ID IS NOT A THING!" });
+    });
+}
--- data/Routers/projectRouter.js
+++ data/Routers/projectRouter.js
@@ -4,13 +4,13 @@ const Project = require("../data/helpers/projectModel");
 
 const router = express.Router();
 
-//Get
+//Get request.
 router.get("/", (req, res) => {
   Project.get()
     .then(project => res.status(200).json(project))
-    .catch(err => res.status(500).json({ message: "YOU CANT HAVE THE PROJECT!" }));
+    .catch(err =>
+      res.status(500).json({ message: "YOU CANT HAVE THE PROJECT!" })
+    );
 });
-
-
 
 module.exports = router;
