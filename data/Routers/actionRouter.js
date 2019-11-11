38c6fc9be2952061b093b1f39eb4741924466659
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-11-11 16:06:24 -0600

    Put and Delete for actions

--- data/Routers/actionRouter.js
+++ data/Routers/actionRouter.js
@@ -30,6 +30,26 @@ router.post("/:id", validateUserId, (req, res) => {
     .catch(err => res.status(500).json({ message: "ERROR WITH ACTION!" }));
 });
 
+//Put with validation
+router.put("/:id", validateActionId, (req, res) => {
+  const postBody = { ...req.body, project_id: req.action.project_id };
+  const id = req.params.id;
+  console.log(postBody);
+  Action.update(id, postBody)
+    .then(action => {
+      res.status(200).json({ message: "UPDATE WORKED!" });
+    })
+    .catch(err => res.status(500).json({ message: "UPDATE DIDNT WORK!" }));
+});
+
+//Delete with validation
+router.delete("/:id", validateActionId, (req, res) => {
+  const id = req.params.id;
+  Action.remove(id)
+    .then(action => res.status(200).json({ message: "YOU DELETED IT!" }))
+    .catch(err => res.status(500).json({ message: "DELETE DIDNT WORK!" }));
+});
+
 //ID Validation middleware
 function validateUserId(req, res, next) {
   const id = req.params.id;
@@ -41,7 +61,7 @@ function validateUserId(req, res, next) {
       } else {
         res
           .status(400)
-          .json({ message: "YOU ARE NOT A REAL USER WITH AN ID@" });
+          .json({ message: "YOU ARE NOT A REAL USER WITH AN ID!" });
       }
     })
     .catch(err => {
